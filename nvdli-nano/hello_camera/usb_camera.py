5dd6f068a6cfe9d4ade83282e6cebab1ee81d6b1
mkhuthir <mkhuthir@gmail.com>
2019-07-13 21:14:27 +0400

    updated hello cam

--- nvdli-nano/hello_camera/usb_camera.py
+++ nvdli-nano/hello_camera/usb_camera.py
@@ -3,6 +3,8 @@
 from jetcam.usb_camera import USBCamera
 
 #TODO change capture_device if incorrect for your system
+#Use included shell script "list_devices.sh" to list connected cameras
+
 camera = USBCamera( width=224, 
                     height=224, 
                     capture_width=640, 
@@ -11,5 +13,5 @@ camera = USBCamera( width=224,
 
 image = camera.read()
 
-print(image.shape)
-print(camera.value.shape)
+print("image shape HxWxC", image.shape)
+print("camera shape HxWxC", camera.value.shape)
