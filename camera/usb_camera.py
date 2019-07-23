9de55d50efab5b4f87e8b78ef1d683452ed0cc43
mkhuthir <mkhuthir@gmail.com>
2019-07-23 23:02:57 +0400

    updated csi output

--- camera/usb_camera.py
+++ camera/usb_camera.py
@@ -5,10 +5,10 @@ from jetcam.usb_camera import USBCamera
 #TODO change capture_device if incorrect for your system
 #Use included shell script "list_devices.sh" to list connected cameras
 
-camera = USBCamera( width=224, 
-                    height=224, 
-                    capture_width=640, 
-                    capture_height=480, 
+camera = USBCamera( width=224,
+                    height=224,
+                    capture_width=640,
+                    capture_height=480,
                     capture_device=0)
 
 image = camera.read()
